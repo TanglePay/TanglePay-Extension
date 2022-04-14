dfe8cbdda7feea41f72efb5c568bcd6197f9126b
daihanqiao <daihanqiao@sxmaps.com>
2022-04-14 20:56:04 +0800

    v1.2.3 fix historicAddressOutputIds=null

--- src/panels/user/aboutUs/index.js
+++ src/panels/user/aboutUs/index.js
@@ -4,7 +4,7 @@ import { Nav, SvgIcon } from '@/common'
 import { default as logo } from '@tangle-pay/assets/images/logo.png'
 
 export const UserAboutUs = () => {
-    var version = '1.2.0'
+    var version = '1.2.3'
     const list = [
         {
             label: I18n.t('user.versionUpdate'),
