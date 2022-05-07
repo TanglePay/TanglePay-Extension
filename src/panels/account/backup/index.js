2c1c133921ee9c423f5d82321fb1be1a5e820dfe
daihanqiao <daihanqiao@sxmaps.com>
2022-05-07 17:22:32 +0800

    fix backup style

--- src/panels/account/backup/index.js
+++ src/panels/account/backup/index.js
@@ -3,8 +3,10 @@ import { Button } from 'antd-mobile'
 import { I18n } from '@tangle-pay/common'
 import { TipsDialog } from './tipsDialog'
 import { Nav, SvgIcon } from '@/common'
+import { useStore } from '@tangle-pay/store'
 
 export const AccountBackup = () => {
+    const [lang] = useStore('common.lang')
     const dialogRef = useRef()
     const handleNext = () => {
         dialogRef.current.show()
@@ -13,14 +15,14 @@ export const AccountBackup = () => {
         <div className='page'>
             <Nav title={I18n.t('account.backupTitle')} />
             <div>
-                <div className='flex c pv70'>
+                <div className={`flex c ${lang == 'de' ? 'pv40' : 'pv70'}`}>
                     <SvgIcon size={70} name='encrypt' className='cB' />
                 </div>
                 <div className='ph50 pb30'>
                     <div className='mb30'>
                         <div className='fz16 fw500'>{I18n.t('account.backupTips')}</div>
                     </div>
-                    <div style={{ marginBottom: 100 }}>
+                    <div style={{ marginBottom: lang == 'de' ? 80 : 100 }}>
                         <div
                             className='fz14'
                             style={{ lineHeight: '20px' }}
