857720d6b6d7bc15c34eb5a4f93916063c175fe2
daihanqiao <daihanqiao@126.com>
2022-08-28 22:23:04 +0800

    update react-scripts

--- bin/cover.js
+++ bin/cover.js
@@ -2,5 +2,12 @@ const fs = require('fs')
 const path = require('path')
 const temPath = path.join(__dirname, './template/iota-next-index-browser.js')
 const toPath = path.join(__dirname, '../node_modules/@iota/iota.js-next/dist/cjs/index-browser.js')
-
 fs.copyFileSync(temPath, toPath)
+
+const temIotaPPath = path.join(__dirname, './template/iota-package.json')
+const toIotaPPath = path.join(__dirname, '../node_modules/@iota/iota.js/package.json')
+fs.copyFileSync(temIotaPPath, toIotaPPath)
+
+const temIotaNextPPath = path.join(__dirname, './template/iota-next-package.json')
+const toIotaNextPPath = path.join(__dirname, '../node_modules/@iota/iota.js-next/package.json')
+fs.copyFileSync(temIotaNextPPath, toIotaNextPPath)
