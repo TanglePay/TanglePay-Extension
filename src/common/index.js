14071b4769099e7fdf9d000921ce64d8643dfd5b
mengyuancmu <raslinyuan@gmail.com>
2023-05-09 11:55:33 +0800

    pin length update plus send confirmation popupp

--- src/common/index.js
+++ src/common/index.js
@@ -1,4 +1,4 @@
 import './style/base.style.less'
 export { SvgIcon } from './assets'
 export { StorageFacade } from './MiscHelper'
-export { Nav, AssetsNav, PasswordDialog, StakingTokenItem, NoData, Toast, MaskedInput } from './components'
+export { Nav, AssetsNav, PasswordDialog, StakingTokenItem, NoData, Toast, MaskedInput, ConfirmDialog } from './components'
