310836b85dc76eb156a2b97dbe4e89a6f4d5ef17
unknown <alexmengyuan2022@gmail.com>
2023-05-24 20:43:10 +0800

    add uuid logic for pin tmp storage

--- src/common/index.js
+++ src/common/index.js
@@ -1,4 +1,5 @@
 import './style/base.style.less'
 export { SvgIcon } from './assets'
 export { StorageFacade } from './MiscHelper'
+export { default as bridge } from "./bridge"
 export { Nav, AssetsNav, PasswordDialog, StakingTokenItem, NoData, Toast, MaskedInput, ConfirmDialog } from './components'
